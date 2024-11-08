6728d7c5eab5f6b69c648f3e796f589c7fb8c718
mlsamuelson <mlsamuel@asu.edu>
2024-11-08 12:47:53 -0700

    fix(unity-react-core): improve factoring of data layer render util

--- shared/utils/datalayer-render.js
+++ shared/utils/datalayer-render.js
@@ -1,23 +1,21 @@
 // @ts-check
 import { trackGAEvent } from "../services/googleAnalytics";
 
+const convertToDataAttr = d => ({
+  "data-ga": d.text,
+  "data-ga-name": d.name,
+  "data-ga-event": d.event,
+  "data-ga-action": d.action,
+  "data-ga-type": d.type,
+  "data-ga-region": d.region,
+  "data-ga-section": d.section,
+  "data-ga-component": d.component,
+});
+
+const convertToClickHandler = d => ({ onClick: e => trackGAEvent(d) });
+
 export function dataLayerRender(gaObject, useExternal) {
-  if (useExternal) {
-    return {
-      "data-ga": gaObject.text,
-      "data-ga-name": gaObject.name,
-      "data-ga-event": gaObject.event,
-      "data-ga-action": gaObject.action,
-      "data-ga-type": gaObject.type,
-      "data-ga-region": gaObject.region,
-      "data-ga-section": gaObject.section,
-      "data-ga-component": "",
-    }
-  } else {
-    return {
-      onClick: (e) => {
-        trackGAEvent(gaObject);
-      },
-    };
-  }
+  return useExternal
+    ? convertToDataAttr(gaObject)
+    : convertToClickHandler(gaObject);
 }

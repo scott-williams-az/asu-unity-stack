0f5d3434d2a0dcc7a81eba0c40d2ed1e35f6ffc4
Scott Williams <stwilli2@asu.edu>
2024-12-12 15:17:17 -0700

    chore(unity-react-core): update plop

--- packages/unity-react-core/plop/component/index.js
+++ packages/unity-react-core/plop/component/index.js
@@ -1,3 +1,56 @@
+const actions = {
+  addTsx: {
+    type: "add",
+    path: "src/components/{{properCase componentName}}/{{properCase componentName}}.tsx",
+    templateFile: "plop/component/component-ts.txt",
+  },
+  addExampleHtml: {
+    type: "add",
+    path: "examples/{{dashCase componentName}}.html",
+    templateFile: "plop/general/example-html.txt",
+  },
+  appendExampleHtmlNav: {
+    type: "append",
+    path: "examples/js/example-helper.js",
+    pattern: /(<nav class=\"nav\" aria-label=\"Same Page\">\n)/,
+    separator: "",
+    template: '                <a class="nav-link" href="{{dashCase componentName}}.html"><span class="fas fa-hand-spock"></span>{{properCase componentName}}</a>\n',
+  },
+  addTsxStories: {
+    type: "add",
+    path: "src/components/{{properCase componentName}}/{{properCase componentName}}.stories.tsx",
+    templateFile: "plop/general/component-stories.txt",
+  },
+  addTsxTests: {
+    type: "add",
+    path: "src/components/{{properCase componentName}}/{{properCase componentName}}.test.tsx",
+    templateFile: "plop/general/component-tests.txt",
+  },
+  addInit: {
+    type: "add",
+    path: "src/components/{{properCase componentName}}/init.js",
+    template: 'export { init{{properCase componentName}} as default } from "../../core/utils";',
+  },
+  appendExportComponents: {
+    type: "append",
+    path: "src/components/index.js",
+    pattern: /(\n$)/gm,
+    separator: "",
+    template: 'export * from "./{{properCase componentName}}/{{properCase componentName}}.tsx";\n',
+  },
+  appendExportCoreUtil: {
+    type: "append",
+    path: "src/core/utils/index.js",
+    pattern: /(\n$)/,
+    separator: "",
+    template: 'import { {{properCase componentName}} } from "../../components/{{properCase componentName}}/{{properCase componentName}}.tsx";\n\n' +
+      ' // @param {ComponentProps} props\n' +
+      'export const init{{properCase componentName}} = ({ targetSelector, props }) =>\n' +
+      '  RenderReact({{properCase componentName}}, props, document.querySelector(targetSelector));\n\n',
+
+  }
+}
+
 module.exports = plop => {
   plop.setGenerator("Typescript Component", {
     description: "New unity TS component",
@@ -10,61 +63,18 @@ module.exports = plop => {
       },
     ],
     actions: [
-      {
-        type: "add",
-        path: "src/components/{{properCase componentName}}/{{properCase componentName}}.tsx",
-        templateFile: "plop/component/component-ts.txt",
-      },
-      {
-        type: "add",
-        path: "examples/{{dashCase componentName}}.html",
-        templateFile: "plop/general/example-html.txt",
-      },
-      {
-        type: "append",
-        path: "examples/js/example-helper.js",
-        pattern: /(<nav class=\"nav\" aria-label=\"Same Page\">\n)/,
-        separator: "",
-        template: '                <a class="nav-link" href="{{dashCase componentName}}.html"><span class="fas fa-hand-spock"></span>{{properCase componentName}}</a>\n',
-      },
-      {
-        type: "add",
-        path: "src/components/{{properCase componentName}}/{{properCase componentName}}.stories.tsx",
-        templateFile: "plop/general/component-stories.txt",
-      },
-      {
-        type: "add",
-        path: "src/components/{{properCase componentName}}/{{properCase componentName}}.test.tsx",
-        templateFile: "plop/general/component-tests.txt",
-      },
-      {
-        type: "add",
-        path: "src/components/{{properCase componentName}}/index.js",
-        template: 'export { init{{properCase componentName}} as default } from "../../core/utils";',
-      },
-      {
-        type: "append",
-        path: "src/components/index.js",
-        pattern: /(\n$)/gm,
-        separator: "",
-        template: 'export * from "./{{properCase componentName}}/{{properCase componentName}}.tsx";\n',
-      },
-      {
-        type: "append",
-        path: "src/core/utils/index.js",
-        pattern: /(\n$)/,
-        separator: "",
-        template: 'import { {{properCase componentName}} } from "../../components/{{properCase componentName}}/{{properCase componentName}}.tsx";\n\n' +
-          ' // @param {ComponentProps} props\n' +
-          'export const init{{properCase componentName}} = ({ targetSelector, props }) =>\n' +
-          '  RenderReact({{properCase componentName}}, props, document.querySelector(targetSelector));\n\n',
-
-      }
+      actions.addTsx,
+      actions.addExampleHtml,
+      actions.appendExampleHtmlNav,
+      actions.addTsxStories,
+      actions.addTsxTests,
+      actions.addInit,
+      actions.appendExportComponents,
+      actions.appendExportCoreUtil,
     ],
   });
-  return; // I don't think we need to support this
-  plop.setGenerator("Component", {
-    description: "New unity component",
+  plop.setGenerator("Typescript Component Example only", {
+    description: "New unity TS component Example only",
     prompts: [
       {
         type: "input",
@@ -73,67 +83,6 @@ module.exports = plop => {
           "What is the name of your component? (use spaces if multi word)",
       },
     ],
-    actions: [
-      {
-        type: "add",
-        path: "src/components/{{properCase componentName}}/{{properCase componentName}}.jsx",
-        templateFile: "plop/component/component-js.txt",
-      },
-      {
-        type: "add",
-        path: "src/components/{{properCase componentName}}/{{properCase componentName}}.styles.js",
-        templateFile: "plop/component/component-styles-js.txt",
-      },
-      {
-        type: "add",
-        path: "src/core/types/{{dashCase componentName}}-types.js",
-        templateFile: "plop/component/component-types-js.txt",
-      },
-      {
-        type: "add",
-        path: "examples/{{dashCase componentName}}.html",
-        templateFile: "plop/general/example-html.txt",
-      },
-      {
-        type: "append",
-        path: "examples/js/example-helper.js",
-        pattern: /(<nav class=\"nav\" aria-label=\"Same Page\">\n)/,
-        separator: "",
-        template: '                <a class="nav-link" href="{{dashCase componentName}}.html"><span class="fas fa-hand-spock"></span>{{properCase componentName}}</a>\n',
-      },
-      {
-        type: "add",
-        path: "src/components/{{properCase componentName}}/{{properCase componentName}}.stories.jsx",
-        templateFile: "plop/general/component-stories.txt",
-      },
-      {
-        type: "add",
-        path: "src/components/{{properCase componentName}}/{{properCase componentName}}.test.jsx",
-        templateFile: "plop/general/component-tests.txt",
-      },
-      {
-        type: "add",
-        path: "src/components/{{properCase componentName}}/index.js",
-        template: 'export { init{{properCase componentName}} as default } from "../../core/utils";',
-      },
-      {
-        type: "append",
-        path: "src/components/index.js",
-        pattern: /(\n$)/gm,
-        separator: "",
-        template: 'export * from "./{{properCase componentName}}/{{properCase componentName}}";\n',
-      },
-      {
-        type: "append",
-        path: "src/core/utils/index.js",
-        pattern: /(\n$)/,
-        separator: "",
-        template: 'import { {{properCase componentName}} } from "../../components/{{properCase componentName}}/{{properCase componentName}}";\n\n' +
-          ' // @param {ComponentProps} props\n' +
-          'export const init{{properCase componentName}} = ({ targetSelector, props }) =>\n' +
-          '  RenderReact({{properCase componentName}}, props, document.querySelector(targetSelector));\n\n',
-
-      }
-    ],
+    actions: [actions.addTsx, actions.addTsxStories, actions.addTsxTests],
   });
 };

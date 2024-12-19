03f6ba5884e53e42761e1d2148b24a7ecfd75ce1
david ornelas <davidornelas_11@hotmail.com>
2024-12-19 12:44:31 -0700

    docs(unity-bootstrap-theme): update Cassie cookie consent

--- packages/unity-bootstrap-theme/stories/molecules/cookie-consent/cookie-consent-full-screen.stories.js
+++ packages/unity-bootstrap-theme/stories/molecules/cookie-consent/cookie-consent-full-screen.stories.js
@@ -1,27 +1,11 @@
 import React, { useEffect } from "react";
-import { allCookieConsentJS } from "../../../src/js/cookie-consent-full-screen.js";
+import { forceReloadOfStory } from "../../../.storybook/decorators";
+import { allCookieConsentJS } from "../../../src/js/cookie-consent-full-screen";
 import "../../../src/css/cookie-consent-full-screen.css";
 
 export default {
   title: "Molecules/Cookie Consent",
-  // Forces reload of the story to keep css and js from persisting between stories
-  decorators: [
-    (storyFn, context) => {
-      useEffect(() => {
-        // document.querySelector("body")?.classList.add("bg-gray-2");
-        setTimeout(() => {
-          context.globals.shouldReload = true;
-        }, 100);
-        return () => {
-          // document.querySelector("body")?.classList.remove("bg-gray-2");
-          if (context.globals.shouldReload) {
-            window.location.reload();
-          }
-        };
-      }, []);
-      return storyFn();
-    },
-  ],
+  decorators: [forceReloadOfStory],
   parameters: { controls: { disable: true } },
 };
 export const FullScreenBannerCookieConsent = () => {
--- packages/unity-bootstrap-theme/stories/molecules/cookie-consent/cookie-consent.stories.js
+++ packages/unity-bootstrap-theme/stories/molecules/cookie-consent/cookie-consent.stories.js
@@ -1,25 +1,11 @@
 import React, { useEffect } from "react";
 import { allCookieConsentJS } from "../../../src/js/cookie-consent";
+import { forceReloadOfStory } from "../../../.storybook/decorators";
 import "../../../src/css/cookie-consent.css";
 
 export default {
   title: "Molecules/Cookie Consent",
-  // Forces reload of the story to keep css and js from persisting between stories
-  decorators: [
-    (storyFn, context) => {
-      useEffect(() => {
-        setTimeout(() => {
-          context.globals.shouldReload = true;
-        }, 100);
-        return () => {
-          if (context.globals.shouldReload) {
-            window.location.reload();
-          }
-        };
-      }, []);
-      return storyFn();
-    },
-  ],
+  decorators: [forceReloadOfStory],
   parameters: { controls: { disable: true } },
 };
 export const MaxWidthCookieConsent = () => {
